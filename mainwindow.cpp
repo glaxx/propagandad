70723f1f8af64a11ab7ac7e7a483884bf2724f44
Stefan Luecke <glaxx@glaxx.net>
2016-07-09 17:04:17 +0200

    Added page switch feature

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,32 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    web = new QWebEngineView(this);
+
+    firstP = new QWebEnginePage(this);
+    firstP->load(QUrl("http://heise.de"));
+
+    secondP = new QWebEnginePage(this);
+    secondP->load(QUrl("http://google.de"));
+
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(switchPages()));
+    timer->start(10000);
+
+    web->setPage(firstP);
+    web->show();
+    ui->horizontalLayout->addWidget(web);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::switchPages() {
+    if (web->page() == firstP) {
+        web->setPage(secondP);
+    } else {
+        web->setPage(firstP);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWebEngineView>
+#include <QWebEnginePage>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void switchPages();
+
 private:
     Ui::MainWindow *ui;
+    QWebEngineView *web;
+    QWebEnginePage *firstP;
+    QWebEnginePage *secondP;
+    QTimer *timer;
 };
 
 #endif // MAINWINDOW_H
